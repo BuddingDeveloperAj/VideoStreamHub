9b31dca40d63e83f79cb49d873dd334f094f1529
Ajay Ravi <ajayravi2208@gmail.com>
2023-09-18 23:15:22 +0530

    Deleting config.js push

--- src/Components/TagsList.js
+++ src/Components/TagsList.js
@@ -1,3 +1,5 @@
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react'
 const listData = [
     "All",
@@ -16,22 +18,66 @@ const listData = [
     "Bollywood",
     "Kollywood",
     "Javascript",
+    "Music",
+    "Tamil Cinema",
+    "Bollywood",
+    "Kollywood",
+    "Javascript",
     "Music"
 ]
 
 const TagsList = () => {
+    const [scrollX, setScrollX] = useState(0);
 
-    const [selectedIndex, setIndex] = useState(null)
+    const handleScrollLeft = () => {
+        const container = document.getElementById('tagsListContainer');
+        if (container) {
+            container.scrollLeft -= 100; // You can adjust the scroll amount
+        }
+    };
 
-    const handleButton = (index) => {
-        setIndex(index)
-    }
+    const handleScrollRight = () => {
+        const container = document.getElementById('tagsListContainer');
+        if (container) {
+            container.scrollLeft += 100; // You can adjust the scroll amount
+        }
+    };
 
     return (
-        <div className='m-2 ml-6 flex'>
-            {listData.map((list, ind) => <button key={ind} onClick={() => handleButton(ind)} className={`hover:bg-gray-800 hover:text-white py-2 px-2 m-1 rounded-lg bg-gray-200 ${selectedIndex === ind ? 'bg-gray-800 text-white' : ''}`}>{list}</button>)}
-        </div>
-    )
-}
+        <div className='flex items-center'>
+            <button
+                onClick={handleScrollLeft}
+                className={`ml-2 p-2 rounded-lg bg-gray-100 hover:bg-gray-500 hover:text-white ${scrollX > 0 ? '' : 'hidden'
+                    }`}
+            >
+                <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <div
+                id='tagsListContainer'
+                className='m-2 ml-6 flex overflow-x-hidden'
+                style={{
+                    maxWidth: '',
+                    whiteSpace: 'nowrap',
+                }}
+            >
+                {listData.map((list, ind) => (
+                    <button
+                        key={ind}
+                        className={`hover:bg-gray-800 flex items-center hover:text-white px-4 m-1 rounded-lg bg-gray-200`}
+                    >
+                        {list}
+                    </button>
+                ))}
+            </div>
 
-export default TagsList;
+            <button
+                onClick={handleScrollRight}
+                className={`ml-2 p-2 rounded-lg bg-gray-100 hover:bg-gray-500 hover:text-white`}
+            >
+                <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+        </div>
+    );
+};
+
+export default TagsList; 
