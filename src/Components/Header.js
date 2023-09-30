3785e14c03cf999f5a862efb3c3405bc995431a0
Ajay Ravi <ajayravi2208@gmail.com>
2023-09-30 21:07:25 +0530

    UPDATE: suggestions url

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -50,7 +50,12 @@ const Header = () => {
                 suggestions = cache[searchText]
             }
             else {
-                let suggestionData = await fetch(YOUTUBE_SUGGESTION_URL + searchText)
+                let suggestionData = await fetch(YOUTUBE_SUGGESTION_URL + searchText, {
+                    mode: 'cors',
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                })
                 let data = await suggestionData.text()
                 data.split('[').forEach((ele, index) => {
                     if (!ele.split('"')[1] || index === 1) return;
